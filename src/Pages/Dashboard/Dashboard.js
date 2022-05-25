6df5b3000fca4e3650dc0b506d66fb08f8daf811
danielNewaz <danielnwz420@gmail.com>
2022-05-25 06:49:46 -0700

    admin added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import useAdmin from './../../hooks/useAdmin';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from './../../firebase.init';
 
 const Dashboard = () => {
+    const [user] = useAuthState(auth);
+    const [admin] = useAdmin(user);
     return (
         <div class="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
@@ -17,7 +22,7 @@ const Dashboard = () => {
                     <li><Link to="/dashboard">My Orders</Link></li>
                     <li><Link to="/dashboard/review">My Reviews</Link></li>
                     <li><Link to="/dashboard/myprofile">My Profile</Link></li>
-                    <li><Link to="/dashboard/users">All Users</Link></li>
+                    { admin && <li><Link to="/dashboard/users">All Users</Link></li>}
                 </ul>
 
             </div>
