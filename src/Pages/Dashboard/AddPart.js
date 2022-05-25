c14a686d2cf1198120a8c85be7cfd33b8a67e231
danielNewaz <danielnwz420@gmail.com>
2022-05-25 15:29:09 -0700

    part addeded

--- src/Pages/Dashboard/AddPart.js
+++ src/Pages/Dashboard/AddPart.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 const AddPart = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+
+    const imageStorageKey = '1acc75709652536edea929de597a8fba'
 
     const onSubmit = async data => {
-        console.log('data', data);
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res=>res.json())
+        .then(result =>{
+            if(result.success){
+                const img = result.data.url;
+                const part = {
+                    name: data.name,
+                    description: data.description,
+                    availableQuantity: data.availableQuantity,
+                    minimumOrder: data.minimumOrder,
+                    price: data.price,
+                    img: img
+                }
+                fetch('http://localhost:5000/tool', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    },
+                    body: JSON.stringify(part)
+                })
+                .then(res => res.json())
+                .then(inserted => {
+                    if(inserted.insertedId){
+                        toast.success('Part added successfully')
+                        reset();
+                    }
+                    else{
+                        toast.error('failed to add Part')
+                    }
+                })
+            }
+          
+        })
     }
 
     return (
@@ -18,7 +61,7 @@ const AddPart = () => {
                         <span className="label-text">Name</span>
                     </label>
                     <input
-                        type="text"
+                        type="Name"
                         placeholder="Name" 
                         className="input input-bordered w-full max-w-xs"
                         {...register("name", {
@@ -59,8 +102,27 @@ const AddPart = () => {
                     </label>
                     <input
                         type="text"
-                        placeholder="Minimum Quantity" className="input input-bordered w-full max-w-xs"
-                        {...register("name", {
+                        placeholder="Available Quantity" className="input input-bordered w-full max-w-xs"
+                        {...register("availableQuantity", {
+                            required: {
+                                value: true,
+                                message: 'Quantity is Required'
+                            }
+                        })}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                    </label>
+                </div>
+                        {/* MinimumOrder */}
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Minimum Order</span>
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Minimum Order" className="input input-bordered w-full max-w-xs"
+                        {...register("minimumOrder", {
                             required: {
                                 value: true,
                                 message: 'Quantity is Required'
@@ -79,10 +141,29 @@ const AddPart = () => {
                     <input
                         type="text"
                         placeholder="Price" className="input input-bordered w-full max-w-xs"
-                        {...register("Price", {
+                        {...register("price", {
                             required: {
                                 value: true,
                                 message: 'Price is Required'
+                            }
+                        })}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                    </label>
+                </div>
+                {/* Photo */}
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <input
+                        type="file"
+                        placeholder="Price" className="input input-bordered w-full max-w-xs"
+                        {...register("image", {
+                            required: {
+                                value: true,
+                                message: 'Image is Required'
                             }
                         })}
                     />
